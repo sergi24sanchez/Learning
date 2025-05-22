3a0183407dd02d39acc6d6a5c9240375f28d13da
Sergi Sánchez <sergi24sanchez@gmail.com>
2025-05-22 08:06:12 +0000

    commit to save past changes

--- nn_zero_to_hero/lectures/build-nanoGPT/train_gpt2.py
+++ nn_zero_to_hero/lectures/build-nanoGPT/train_gpt2.py
@@ -232,23 +232,35 @@ class GPT(nn.Module):
 
 # ---------------------------------------------------------------------------
 import tiktoken
+import numpy as np
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes): # if process_rank=0, num_processes=1 --> it is the same as 1 GPU
+    def __init__(self, B, T, process_rank, num_processes, split): # if process_rank=0, num_processes=1 --> it is the same as 1 GPU
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
+        assert split in {'train', 'val'}
 
-        # at init load tokens from disk and store them in memory
-        with open('input.txt', 'r') as f:
-            text = f.read()
-        enc = tiktoken.get_encoding('gpt2')
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"loaded {len(self.tokens)} tokens")
-        print(f" 1 epoch = {len(self.tokens) // (B * T)} batches")
-    
-        #state
+        # get the shar filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
     
     def next_batch(self):
@@ -260,6 +272,8 @@ class DataLoaderLite:
         self.current_position += B * T * self.num_processes
         # if loading the next batch would be out of bounds, reset
         if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
             self.current_position = self.B * self.T * self.process_rank # reset back the same way
         return x, y
 
@@ -318,7 +332,7 @@ if master_process:
     print(f"=> calculated gradient accumulaition steps: {grad_accum_steps}")
 
 # We don't want the processes to run the exact same data. All working on different chunks of data
-train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_local_rank, num_processes=ddp_world_size)
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_local_rank, num_processes=ddp_world_size, split='train')
 
 torch.set_float32_matmul_precision('high') # matrix multiplications uste TensorFloat32 datatype
 
@@ -339,8 +353,9 @@ raw_model = model.module if ddp else model # always contains the "raw" unwrapped
 
 max_lr = 6e-4
 min_lr = max_lr * 0.1
-warmmup_steps = 10
-max_steps = 50
+warmmup_steps = 715 # gpt3 paper says warmup lr over the first 375M tokens --> 375e6 / 2^19 = 715 steps
+max_steps = 19073 # 10B tokens / 2^19 batch size = 19073 steps
+
 def get_lr(it):
     # 1. linear warmup for warmup_iters steps
     if it < warmmup_steps:
